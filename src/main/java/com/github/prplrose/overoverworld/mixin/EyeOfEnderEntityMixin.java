0d9c72cb226b659edaabc56550f083e05385ecbb
prplrose <158543118+prplrose@users.noreply.github.com>
2024-02-17 21:05:36 +0100

    removed not used classes and imports

--- src/main/java/com/github/prplrose/overoverworld/mixin/EyeOfEnderEntityMixin.java
+++
@@ -1,8 +0,0 @@
-package com.github.prplrose.overoverworld.mixin;
-
-import net.minecraft.entity.EyeOfEnderEntity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(EyeOfEnderEntity.class)
-public class EyeOfEnderEntityMixin {
-}
